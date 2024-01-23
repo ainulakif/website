18ddc89c8a260f265e3856ddd99a45789d272abc
Ainul Akif <ainulakif2@gmail.com>
2024-01-23 12:39:10 +0800

    setup nextapi/route using Google

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,38 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // images: {
+    //     domains: ['lh3.googleusercontent.com'],
+    // },
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: '**.googleusercontent.com',
+            port: '',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     experimental: {
+//         appDir: true,
+//         serverComponentsExternalPackages: ["mongoose"],
+//     },
+//     images: {
+//         domains: ['lh3.googleusercontent.com'],
+//     },
+//     webpack(config) {
+//         config.experiments = {
+//             ...config.experiments,
+//             topLevelAwait: true,
+//         }
+//         return config
+//     }
+// }
+
+// module.exports = nextConfig
+// export default nextConfig;

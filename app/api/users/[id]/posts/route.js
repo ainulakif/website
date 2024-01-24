b7e3bf89de16ce8947ad6db78863c90e842b1bb0
Ainul Akif <ainulakif2@gmail.com>
2024-01-24 15:31:04 +0800

    setup profile prompt feed

---
+++ app/api/users/[id]/posts/route.js
@@ -0,0 +1,20 @@
+//same as prompt/route
+//modified to fetch the specific creators
+
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const GET = async (request, { params }) => {
+    try {
+        await connectToDB();
+
+        const prompts = await Prompt.find({
+            //[id] as dynamic paramter
+            creator: params.id
+        }).populate('creator');
+
+        return new Response(JSON.stringify(prompts), { status: 200 })
+    } catch (error) {
+        return new Response("Failed to fetch all prompts", { status: 500 })
+    }
+}

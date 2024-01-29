9af6b9e8da91d772301cafe78dfc704245467857
Ainul Akif <ainulakif2@gmail.com>
2024-01-29 15:54:02 +0800

    Entire page "/update-prompt" deopted into client-side rendering

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -3,6 +3,8 @@
 import { useState, useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 
+import { Suspense } from 'react';
+
 import Form from "@components/Form";
 
 const EditPrompt = () => {
@@ -55,16 +57,19 @@ const EditPrompt = () => {
             setSubmitting(false);
         }
     }
+    
 
     return (
 
-        <Form
-            type="Edit"
-            post={post}
-            setPost={setPost}
-            submitting={submitting}
-            handleSubmit={updatePrompt}
-        />
+        <Suspense fallback={<>Loading...</>}>
+            <Form
+                type="Edit"
+                post={post}
+                setPost={setPost}
+                submitting={submitting}
+                handleSubmit={updatePrompt}
+            />
+        </Suspense>
     )
 }
 

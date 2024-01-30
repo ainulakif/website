213bd858440a229cfce1f5d4de05f86e3277b81e
Ainul Akif <ainulakif2@gmail.com>
2024-01-30 16:21:22 +0800

    Updated Portfolio and create new href to a branched project

--- app/page.jsx
+++ app/page.jsx
@@ -5,12 +5,12 @@ const Home = () => {
     <section className="w-full flex-center flex-col">
         <h1 className="head_text head_center">
             {/* underscore means globals.css */}
-            Discover & Share
+           My Web Portfolio
             <br className="max-md:hidden" />
-            <span className="green_gradient text-center"> AI-Powered Prompts</span>
+            <span className="green_gradient text-center"> Multiple Projects</span>
         </h1>
         <p className="desc text-center">
-            Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts
+        My web portfolio showcases a collection of my latest projects. Explore the diverse range of projects to get a glimpse of my capabilities and experience in the field of web development.
         </p>
 
         <Feed />

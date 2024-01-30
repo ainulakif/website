a968b159281eddf18377c61e61234c0a58e89323
Ainul Akif <ainulakif2@gmail.com>
2024-01-30 15:57:26 +0800

    Integrated weather app

--- components/Weather.jsx
+++ components/Weather.jsx
@@ -1,51 +1,39 @@
 "use client";
 
-import Image from "next/image"
+import Searchbar from "./Searchbar";
+import { useState } from "react";
+import CurrentWeather from "./CurrentWeather";
 
 function Weather() {
+
+    const [currentWeather, setCurrentWeather] = useState(null);
+
+    const handleOnSearchChange = (searchData) => {
+        const [lat, lon] = searchData.value.split(" ");
+
+        const currentWeatherFetch = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=c01ea3935a9b996af4b66cc728c6bb2f&units=metric`);
+
+        Promise.all([currentWeatherFetch])
+            .then(async (response) => {
+                const weatherResponse = await response[0].json();
+
+                // console.log("Response: ", weatherResponse);
+
+                setCurrentWeather({ city: searchData.label, ...weatherResponse });
+            })
+            .catch((err) => console.log(err));
+    }
+
+    console.log("Current Weather Response: ", currentWeather);
+
     return (
         <div className="prompt_card">
-            <div className="flex justify-between items-start gap-5">
-                <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
-                    <Image
-                        src=""
-                        alt="user_image"
-                        width={40}
-                        height={40}
-                        className="rounded-full object-contain"
-                    />
+            <Searchbar onSearchChange={handleOnSearchChange} />
+           {currentWeather && <CurrentWeather data={currentWeather} />}
 
-                    <div className="flex flex-col">
-                        <h3 className="font-satoshi font-semibold text-gray-900">
-                            Test1
-                        </h3>
-                        <p className="font-inter text-sm text-gray-500">
-                            Test2
-                        </p>
-                    </div>
-                </div>
-
-                <div className="copy_btn" onClick={() => {}}>
-                    <Image
-                        src="/assets/icons/copy.svg"
-                        alt="copy_btn"
-                        width={12}
-                        height={12}
-                    />
-                </div>
-            </div>
-
-            <p className="my-4 font-satoshi text-sm text-gray-700">
-                Test3
-            </p>
-            <p className="font-inter text-sm blue_gradient cursor-pointer"
-                onClick={() => { }}
-            >
-                TEST4
-            </p>
 
             {/* {session?.user.id === post.creator._id && pathName === "/profile" && ( */}
-            <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+            {/* <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
                 <p
                     className="font-inter text-sm green_gradient cursor-pointer"
                     onClick={() => { }}
@@ -58,7 +46,7 @@ function Weather() {
                 >
                     Delete
                 </p>
-            </div>
+            </div> */}
             {/* )} */}
         </div>
     )

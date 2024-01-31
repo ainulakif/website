a7e8de83ea38d5c97dd52aafabb5618c42b32c10
Ainul Akif <ainulakif2@gmail.com>
2024-01-31 12:53:44 +0800

    moved weather folder components

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 
 import PromptCard from "./PromptCard";
-import Weather from "./Weather";
+import Weather from "./weather/Weather";
 
 const PrompCardList = ({ data, handleTagClick }) => {
   return (

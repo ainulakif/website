32bc8e62693d5a65ea7e6ed8c4ea7982773676f6
Ainul Akif <ainulakif2@gmail.com>
2024-02-07 12:12:00 +0800

    updated layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,7 @@ import Provider from "@components/Provider";
 import "@styles/globals.css";
 
 export const metadata = {
-    title: "ainulakif",
+    title: "Website",
     description: "Ainul Akif's website"
 }
 

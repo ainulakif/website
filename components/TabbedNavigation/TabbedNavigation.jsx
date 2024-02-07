9a280e94797457eaf60531cf97fca068bb38e9b7
Ainul Akif <ainulakif2@gmail.com>
2024-02-07 11:21:02 +0800

    added chart at homepage

--- components/TabbedNavigation/TabbedNavigation.jsx
+++ components/TabbedNavigation/TabbedNavigation.jsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react';
 import Weather from '@components/weather/Weather';
 import Feed from '@components/Feed';
+import TabbedChart from '@components/countries/TabbedChart';
 
 const TabbedNavigation = () => {
   const [activeTab, setActiveTab] = useState('tab1');
@@ -16,8 +17,9 @@ const TabbedNavigation = () => {
     <div className='tabbed_navigation'>
       <section className='feed'>
         <div className='tab_buttons'>
-          <button onClick={() => handleTabClick('tab1')}>Tab 1</button>
-          <button onClick={() => handleTabClick('tab2')}>Tab 2</button>
+          <button onClick={() => handleTabClick('tab1')}>Weather</button>
+          <button onClick={() => handleTabClick('tab2')}>Prompt</button>
+          <button onClick={() => handleTabClick('tab3')}>Population</button>
         </div>
         {
           activeTab === 'tab1' &&
@@ -26,6 +28,10 @@ const TabbedNavigation = () => {
         {
           activeTab === 'tab2' &&
           <Feed />
+        }
+        {
+          activeTab === 'tab3' &&
+          <TabbedChart />
         }
       </section>
     </div>
---
+++ components/countries/TabbedChart.jsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import Population from "@components/countries/Population";
+import ChartDetails from "@components/countries/ChartDetails";
+
+const TabbedChart = () => {
+
+    const [result, setResult] = useState([])
+
+    useEffect(() => {
+        const getCountriesDetails = async () => {
+            try {
+                const response = await fetch('https://restcountries.com/v3.1/all?fields=name,population')
+                const data = await response.json();
+
+                setResult(data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getCountriesDetails();
+    }, []);
+
+    // console.log(result);
+    // console.log("name: ", result[0].name.common);
+    // console.log("population: ", result[0].population);
+    // console.log("length of array: ", result.length);
+    return (
+        <section className="w-full flex-center flex-col" style={{ height: "80vh" }}>
+            {/* <Population data={{}} /> */}
+            <div>Countries</div>
+            <ChartDetails output={result} />
+        </section>
+    )
+    //   https://restcountries.com/v3.1/all?fields=name,population
+}
+
+export default TabbedChart

5145b1b841d4a8247c8783d6aafaaee091cc806f
Ainul Akif <ainulakif2@gmail.com>
2024-02-14 17:30:13 +0800

    debugged web analytics in vercel

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react";
+
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 import "@styles/globals.css";
@@ -19,6 +21,7 @@ const RootLayout = ({ children }) => {
                     <main className="app">
                         <Nav />
                         {children}
+                        <Analytics />
                     </main>
                 </Provider>
             </body>

6c572d1ab9406be25b37d48a49ed691b525dd7e5
Ainul Akif <ainulakif2@gmail.com>
2024-02-14 18:46:14 +0800

    changed icon for head and navbar

--- README.md
+++ README.md
@@ -51,7 +51,7 @@ A [portfolio website application](https://ainulakif-website.vercel.app/) that I 
 >The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-### [1.5.0] - 2024-02-14
+### [1.5.2] - 2024-02-14
 
 #### Added
 
--- app/layout.jsx
+++ app/layout.jsx
@@ -12,6 +12,9 @@ export const metadata = {
 const RootLayout = ({ children }) => {
     return (
         <html lang="en">
+            <head>
+                <link rel="icon" href="/assets/images/icon-fav.png" sizes="any" />
+            </head>
             <body>
                 <Provider>
                     <div className="main">
